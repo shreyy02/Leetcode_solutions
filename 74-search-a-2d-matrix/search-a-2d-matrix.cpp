6934397511aa79f3c4804c36c4586cfb3e65f8d2
Shreya Singh <145997075+shreyy02@users.noreply.github.com>
2026-08-08 20:20:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (80.06%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size() ;
+        int m = matrix[0].size() ;
+
+        int low = 0 ;
+        int high = m*n -1 ;
+
+        while(low <= high){
+            int guess = (low + high) / 2 ;
+            int row = guess/m ;
+            int col = guess % m ;
+
+            if(matrix[row][col] == target){
+                return true ;
+            }
+            if(matrix[row][col] < target){
+                low = guess + 1 ;
+            }
+            else{
+                high = guess - 1 ;
+            }
+
+            
+        }
+        return false ;
+    }
+};
